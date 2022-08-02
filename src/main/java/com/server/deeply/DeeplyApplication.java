1506f593081175fff9a8338abb33bae9c11f1c4f
yunjinchoidev <yunjinchoidev@gmail.com>
2022-08-02 22:16:16 +0900

    최초 커밋

---
+++ src/main/java/com/server/deeply/DeeplyApplication.java
@@ -0,0 +1,13 @@
+package com.server.deeply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeplyApplication.class, args);
+    }
+
+}
