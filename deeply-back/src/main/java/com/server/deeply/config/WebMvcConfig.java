6f91e410556d2b72c5b598b05b93c962ed928119
yunjin Choi <89494907+yunjinchoidev@users.noreply.github.com>
2022-11-16 03:54:16 +0900

    back - 'Authorization' 헤더 토큰 허용

--- deeply-back/src/main/java/com/server/deeply/config/WebMvcConfig.java
+++ deeply-back/src/main/java/com/server/deeply/config/WebMvcConfig.java
@@ -15,6 +15,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true)
-						.maxAge(MAX_AGE_SECS);
+						.maxAge(MAX_AGE_SECS)
+				 		.exposedHeaders("Authorization");
+
 	}
 }
