a539a265e3a2cbf50ce5d226480e04d789815be6
yunjin Choi <89494907+yunjinchoidev@users.noreply.github.com>
2022-11-21 02:22:47 +0900

    back - kafka 기초 세팅 추가

---
+++ deeply-back/src/main/java/com/server/deeply/util/kafka/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.server.deeply.util.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(
+            topics = "jinmin",
+            groupId = "foo"
+    )
+    public void listen(String msg) throws IOException {
+        System.out.println(String.format("Consumed message : %s", msg));
+    }
+}
