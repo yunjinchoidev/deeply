61a88ce23d1a01d1326352277421223dfd4f4ba7
yunjinchoidev <yunjinchoidev@gmail.com>
2023-01-08 10:56:15 +0900

    back - cors ip 추가

--- deeply-back/src/main/java/com/server/deeply/config/WebMvcConfig.java
+++ deeply-back/src/main/java/com/server/deeply/config/WebMvcConfig.java
@@ -1,4 +1,5 @@
 package com.server.deeply.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -6,18 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	private final long MAX_AGE_SECS = 3600;
+    private final long MAX_AGE_SECS = 3600;
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true)
-						.maxAge(MAX_AGE_SECS)
-				 		.exposedHeaders("Authorization")
-				 		.exposedHeaders("refreshToken");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://15.164.171.59:80")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS)
+                .exposedHeaders("Authorization")
+                .exposedHeaders("refreshToken");
 
-	}
+    }
 }
